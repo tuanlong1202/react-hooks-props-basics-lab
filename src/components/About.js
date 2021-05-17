6eccbce6b6582c63301ca40bcef8c8d9588bcb53
tuanlong1202 <tuanlong1202@gmail.com>
2021-05-16 21:28:52 -0400

    react hooks props basics lab DONE

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(prop) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {(prop.bio === "" || !prop.bio) ? '' : <p>{prop.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
